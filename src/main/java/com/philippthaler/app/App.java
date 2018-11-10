68f633d57e568c471150cbd8e47857fe781b3e7c
Philipp Thaler <thalerphilipp1@gmail.com>
2018-11-10 16:04:59 +0100

    Fixed the app class back to normal

--- src/main/java/com/philippthaler/app/App.java
+++ src/main/java/com/philippthaler/app/App.java
@@ -2,11 +2,6 @@ package com.philippthaler.app;
 
 import com.philippthaler.app.utils.Warehouse;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.util.Properties;
-
 /**
  * Hello world!
  */
@@ -15,18 +10,8 @@ public class App {
   public static final int COLUMN_INIT=10, ROW_INIT = 10;
 
   public static void main(String[] args) {
-    Connection connection = null;
-    Properties connectionProps = new Properties();
-    connectionProps.put("user", "root");
-    connectionProps.put("password", "password");
+    Warehouse warehouse = Warehouse.getInstance();
 
-    try {
-      connection=DriverManager.getConnection("jdbc:mariadb://localhost:3306/",connectionProps);
-    } catch (SQLException e) {
-      e.printStackTrace();
-    }
-    System.out.println("Connected to database");
-
-
+    warehouse.start();
   }
 }
